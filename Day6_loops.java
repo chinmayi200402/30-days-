35a4eba8353ead2385c689f426077ab7aa2da1e2
chinmayi200402 <dchinmayi2004@gmail.com>
2025-08-23 18:17:44 +0530

    Loops

---
+++ Day6_loops.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        for (int i = 1; i <= 10; i++) {
+            int res = n * i;
+            System.out.println(n + " x " + i + " = " + res);
+        }
+
+        bufferedReader.close();
+    }
+}
